9c418843673f2b80122f03c342f0b94d4f02a1f6
appoclayptor <shahreza1414@gmail.com>
2024-11-14 09:48:31 +0600

    first commit

---
+++ src/Layouts/HomeLayout.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../Components/Header';
+
+const HomeLayout = () => {
+    return (
+        <div>
+            <Header></Header>
+            <nav></nav>
+            <main></main>
+        </div>
+    );
+};
+
+export default HomeLayout;

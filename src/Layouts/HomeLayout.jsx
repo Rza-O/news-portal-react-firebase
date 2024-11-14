70835fc4be8b6fce9b8f81f37d50aed3c00723e4
appoclayptor <shahreza1414@gmail.com>
2024-11-14 10:46:31 +0600

    Added navbar and created gid template for the main section

--- src/Layouts/HomeLayout.jsx
+++ src/Layouts/HomeLayout.jsx
@@ -1,16 +1,25 @@
 import React from 'react';
 import Header from '../Components/Header';
 import LatestNews from '../Components/LatestNews';
+import Navbar from '../Components/Navbar';
 
 const HomeLayout = () => {
     return (
-        <div>
-            <Header></Header>
-            <section className='w-11/12 mx-auto'>
-                <LatestNews></LatestNews>
-            </section>
-            <nav></nav>
-            <main></main>
+        <div className='font-poppins'>
+            <header>
+                <Header></Header>
+                <section className='w-11/12 mx-auto'>
+                    <LatestNews></LatestNews>
+                </section>
+            </header>
+            <nav className='w-11/12 mx-auto py-2'>
+                <Navbar></Navbar>
+            </nav>
+            <main className='w-11/12 mx-auto pt-5 grid md:grid-cols-12 gap-3'>
+                <aside className='left col-span-3'>Left navbar</aside>
+                <section className='col-span-6'>Main Content</section>
+                <aside className='col-span-3'>Right Navbar</aside>
+            </main>
         </div>
     );
 };

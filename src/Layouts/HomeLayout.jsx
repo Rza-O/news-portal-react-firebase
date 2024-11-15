64ff71464cc5d66988a0c8e1b75b34ccf4442ed7
appoclayptor <shahreza1414@gmail.com>
2024-11-15 07:13:22 +0600

    Added dynamic button with fetch api

--- src/Layouts/HomeLayout.jsx
+++ src/Layouts/HomeLayout.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from '../Components/Header';
 import LatestNews from '../Components/LatestNews';
 import Navbar from '../Components/Navbar';
+import LeftNavbar from '../Components/LayoutComponents/LeftNavbar';
 
 const HomeLayout = () => {
     return (
@@ -16,7 +17,9 @@ const HomeLayout = () => {
                 <Navbar></Navbar>
             </nav>
             <main className='w-11/12 mx-auto pt-5 grid md:grid-cols-12 gap-3'>
-                <aside className='left col-span-3'>Left navbar</aside>
+                <aside className='left col-span-3'>
+                    <LeftNavbar></LeftNavbar>
+                </aside>
                 <section className='col-span-6'>Main Content</section>
                 <aside className='col-span-3'>Right Navbar</aside>
             </main>
